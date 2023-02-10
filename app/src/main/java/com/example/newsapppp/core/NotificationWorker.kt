02b6d2beeb2bffba03b02ca9677680d4198ad8fd
Stas-8888 <life4musicrd@gmail.com>
2023-02-10 13:00:26 +0200

    Add notification work manager.

---
+++ app/src/main/java/com/example/newsapppp/core/NotificationWorker.kt
@@ -0,0 +1,78 @@
+package com.example.newsapppp.core
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.newsapppp.R
+import com.example.newsapppp.presentation.ui.MainActivity
+
+class NotificationWorker(context: Context, workerParameters: WorkerParameters) : Worker(context, workerParameters) {
+
+    override fun doWork(): Result {
+        showNotification()
+        return Result.success()
+    }
+
+    private fun showNotification() {
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0, intent, 0
+        )
+
+        val notification = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Visit our App. And you will know main news in the world ")
+            .setContentText("EASY News")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Channel Name"
+            val channelDescription = "Channel Description"
+            val channelImportance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, channelName, channelImportance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager = applicationContext.getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        with(NotificationManagerCompat.from(applicationContext)) {
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            notify(NOTIFICATION_ID, notification.build())
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "channel_id"
+        const val NOTIFICATION_ID = 1
+    }
+}
