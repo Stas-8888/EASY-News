a4966964c50ce976d4eec5d643dfbd9912cad29e
Stas-8888 <life4musicrd@gmail.com>
2023-02-15 16:52:02 +0200

    add bottom navigation

--- app/src/main/java/com/example/newsapppp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapppp/presentation/ui/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.newsapppp.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapppp.R
 import com.example.newsapppp.databinding.ActivityMainBinding
+import com.example.newsapppp.presentation.extensions.invisible
+import com.example.newsapppp.presentation.extensions.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,5 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavController()
+    }
+
+    private fun setupNavController() = with(binding) {
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.saveFragment,
+                R.id.mainFragment,
+                R.id.searchFragment -> bottomNavigationView.visible()
+                else -> bottomNavigationView.invisible()
+            }
+        }
     }
 }
