9916401d64cd85d374da1f8c1ab7df6a4aee5e17
Stas-8888 <life4musicrd@gmail.com>
2023-03-13 10:01:05 +0200

    add unit test

---
+++ app/src/test/java/com/example/newsapppp/domain/interactors/articleremote/GetNewsUseCaseTest.kt
@@ -0,0 +1,40 @@
+package com.example.newsapppp.domain.interactors.articleremote
+
+import androidx.paging.PagingData
+import com.example.newsapppp.domain.model.ArticleModel
+import com.example.newsapppp.domain.repository.ArticleRemoteRepositoryContract
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class GetNewsUseCaseTest {
+
+    private val mockRepo = mock<ArticleRemoteRepositoryContract>()
+
+    @Test
+    fun `invoke should return flow of PagingData ArticleModel from repository`() = runBlocking {
+
+        val categoryName = "business"
+        val mockArticleModel = ArticleModel(
+            title = "Mock article title",
+            description = "Mock article description",
+            url = "http://example.com",
+            urlToImage = "http://example.com/image.jpg",
+            publishedAt = "2023-03-08T12:00:00Z",
+            content = "Mock article content",
+            author = "Mock author"
+        )
+        val pagingData = PagingData.from(listOf(mockArticleModel))
+        val pagingSource = flowOf(pagingData)
+
+        whenever(mockRepo.getNews(categoryName)).thenReturn(pagingSource)
+
+        val getNewsUseCase = GetNewsUseCase(mockRepo)
+        val result = getNewsUseCase(categoryName).first()
+        assertEquals(pagingData, result)
+    }
+}
