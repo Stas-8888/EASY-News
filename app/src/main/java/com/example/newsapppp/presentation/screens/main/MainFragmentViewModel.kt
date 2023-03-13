6e3858286083ddf34562d3635b9f6e458764b90e
Stas-8888 <life4musicrd@gmail.com>
2023-03-13 22:56:38 +0200

    refactoring

--- app/src/main/java/com/example/newsapppp/presentation/screens/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/newsapppp/presentation/screens/main/MainFragmentViewModel.kt
@@ -3,6 +3,7 @@ package com.example.newsapppp.presentation.screens.main
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import androidx.paging.filter
+import com.example.newsapppp.R
 import com.example.newsapppp.core.network.NetworkHandlerContract
 import com.example.newsapppp.data.remote.interceptor.ErrorsInterceptorContract
 import com.example.newsapppp.domain.interactors.articleremote.GetNewsUseCase
@@ -33,28 +34,44 @@ class MainFragmentViewModel @Inject constructor(
     override val _state = MutableStateFlow<MainState>(MainState.ShowLoading)
 
     // Sets up UI for main fragment with news for first category
-    fun setupUi() = viewModelScope.launch {
-        val news = getNews(categories.first()).cachedIn(viewModelScope)
-        news.collect { articles ->
-            val filteredArticles = articles.filter { article ->
-                article.urlToImage != null && article.title != null
+    fun fetchAndShowNews() = viewModelScope.launch {
+        val action = MainAction.ShowNetworkDialog(R.string.internet_disconnected)
+        when {
+            network.isNetworkAvailable().not() -> emitAction(action)
+            else -> try {
+                val news = getNews(categories.first()).cachedIn(viewModelScope)
+                news.collect { articles ->
+                    val filteredArticles = articles.filter { article ->
+                        article.urlToImage != null && article.title != null
+                    }
+                    val mappedArticles = mapper.mapToPagingArticle(filteredArticles)
+                    val countryFlag = getCountryFlag(Unit)
+                    emit(MainState.ShowUI(mappedArticles, countryFlag))
+                }
+            } catch (e: Exception) {
+                emitAction(MainAction.ShowMessage(e.message))
             }
-            val mappedArticles = mapper.mapToPagingArticle(filteredArticles)
-            val countryFlag = getCountryFlag(Unit)
-            val uiState = MainState.ShowUI(mappedArticles, countryFlag)
-            emit(uiState)
         }
     }
 
     // Sets up news for a specific tab/category
     fun setupTabLayout(tab: TabLayout.Tab) = viewModelScope.launch {
-        val category = categories[tab.position]
-        val news = getNews(category).cachedIn(viewModelScope)
-        news.collect { articles ->
-            val filteredArticles = articles.filter { it.urlToImage != null && it.title != null }
-            val mappedArticles = mapper.mapToPagingArticle(filteredArticles)
-            val countryFlag = getCountryFlag(Unit)
-            emit(MainState.ShowUI(mappedArticles, countryFlag))
+        val action = MainAction.ShowNetworkDialog(R.string.internet_disconnected)
+        when {
+            network.isNetworkAvailable().not() -> emitAction(action)
+            else -> try {
+                val news = getNews(categories[tab.position]).cachedIn(viewModelScope)
+                news.collect { articles ->
+                    val filteredArticles = articles.filter { article ->
+                        article.urlToImage != null && article.title != null
+                    }
+                    val mappedArticles = mapper.mapToPagingArticle(filteredArticles)
+                    val countryFlag = getCountryFlag(Unit)
+                    emit(MainState.ShowUI(mappedArticles, countryFlag))
+                }
+            } catch (e: Exception) {
+                emitAction(MainAction.ShowMessage(e.message))
+            }
         }
     }
 
