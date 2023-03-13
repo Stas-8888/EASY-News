9e8347160f42babd4772971a5540d56ecce314c7
Stas-8888 <life4musicrd@gmail.com>
2023-03-13 10:02:05 +0200

    add unit test

---
+++ app/src/test/java/com/example/newsapppp/domain/interactors/authentication/SignInUseCaseTest.kt
@@ -0,0 +1,35 @@
+package com.example.newsapppp.domain.interactors.authentication
+
+import com.example.newsapppp.domain.model.UserModel
+import com.example.newsapppp.domain.repository.AuthenticationRepositoryContract
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+class SignInUseCaseTest {
+
+    private val mockRepository = mock<AuthenticationRepositoryContract>()
+    private val useCase = SignInUseCase(mockRepository)
+
+
+    @Test
+    fun `invoke should call signIn method from repository`() = runBlocking {
+        val mockUserModel = UserModel(user = "testuser", password = "testpassword")
+
+        useCase(mockUserModel)
+        verify(mockRepository).signIn(mockUserModel)
+    }
+
+    @Test
+    fun `invoke should return result from repository`() = runBlocking {
+        val mockUserModel = UserModel(user = "testuser", password = "testpassword")
+
+        Mockito.`when`(mockRepository.signIn(mockUserModel)).thenReturn(Unit)
+
+        val result = useCase(mockUserModel)
+        assertEquals(Unit, result)
+    }
+}
