6524d2bb9ba09d085b472f2831e322195778099c
Stas-8888 <life4musicrd@gmail.com>
2023-03-15 21:21:03 +0200

    add comments

--- app/src/main/java/com/example/newsapppp/data/cache/ArticleCache.kt
+++ app/src/main/java/com/example/newsapppp/data/cache/ArticleCache.kt
@@ -10,6 +10,16 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+/**
+ * This is a cache repository for storing and retrieving news article data. It uses the [ArticleDao]
+ * to interact with the local database, the [EntityMapper] to map the article data between the domain
+ * layer and the database layer, the [NewsResponseMapper] to map the article data between the remote
+ * data source and the domain layer, and the [DispatcherRepositoryContract] to perform asynchronous
+ * operations on an I/O-bound coroutine dispatcher.
+ *
+ * This class implements the [ArticleCacheContract] interface and provides methods to insert, delete,
+ * and retrieve articles from the cache.
+ */
 class ArticleCache @Inject constructor(
     private val articleDao: ArticleDao,
     private val mapper: EntityMapper,
@@ -17,18 +27,40 @@ class ArticleCache @Inject constructor(
     private val dispatcher: DispatcherRepositoryContract
 ) : ArticleCacheContract {
 
+    /**
+     * Inserts the specified [article] into the cache. Uses [dispatcher] to perform the operation
+     * on an I/O-bound coroutine dispatcher.
+     *
+     * @param article the article to insert into the cache.
+     */
     override suspend fun insertArticle(article: ArticleModel) = dispatcher.io {
         articleDao.insertArticle(mapper.mapToModel(article))
     }
 
+    /**
+     * Deletes the specified [article] from the cache. Uses [dispatcher] to perform the operation
+     * on an I/O-bound coroutine dispatcher.
+     *
+     * @param article the article to delete from the cache.
+     */
     override suspend fun deleteArticle(article: ArticleModel) = dispatcher.io {
         articleDao.deleteArticle(mapper.mapToModel(article))
     }
 
+    /**
+     * Deletes all articles from the cache. Uses [dispatcher] to perform the operation on an I/O-bound
+     * coroutine dispatcher.
+     */
     override suspend fun deleteAllArticle() = dispatcher.io {
         articleDao.deleteAllArticle()
     }
 
+    /**
+     * Retrieves all articles from the cache as a [Flow] of [List] of [ArticleModel]. Uses
+     * [dispatcher] to perform the operation on an I/O-bound coroutine dispatcher.
+     *
+     * @return a [Flow] of [List] of [ArticleModel] objects that represent the articles.
+     */
     override fun getAllArticles(): Flow<List<ArticleModel>> {
         return articleDao.getAllArticles()
             .map { newsResponseMapper.fromArticleEntityToArticleModel(it) }
--- app/src/main/java/com/example/newsapppp/data/remote/ArticleRemote.kt
+++ app/src/main/java/com/example/newsapppp/data/remote/ArticleRemote.kt
@@ -14,6 +14,16 @@ import com.example.newsapppp.domain.model.NewsResponseModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * This is a remote repository for retrieving news article data. It uses the [ApiService] to fetch
+ * article data from a remote API, and the [NewsResponseMapper] to map the API response to a
+ * [NewsResponseModel]. Additionally, it uses the [SharedPreferences] to get the user's country
+ * flag and the [DispatcherRepositoryContract] to perform asynchronous operations on an I/O-bound
+ * coroutine dispatcher.
+ *
+ * This class implements the [ArticleRemoteContract] interface and provides methods to fetch
+ * articles based on a category and to search for articles based on a search query.
+ */
 class ArticleRemote @Inject constructor(
     private val apiService: ApiService,
     private val mapper: NewsResponseMapper,
@@ -21,6 +31,14 @@ class ArticleRemote @Inject constructor(
     private val dispatcher: DispatcherRepositoryContract,
 ) : ArticleRemoteContract {
 
+    /**
+     * Retrieves a [Flow] of [PagingData] for the specified [category] of news articles from the
+     * remote data source. Uses [dispatcher] to perform the operation on an I/O-bound coroutine
+     * dispatcher.
+     *
+     * @param category - the category of news articles to retrieve.
+     * @return a [Flow] of [PagingData] objects that represent the articles.
+     */
     override suspend fun fetchedArticles(category: String): Flow<PagingData<ArticleModel>> {
         return dispatcher.io {
             Pager(
@@ -32,6 +50,13 @@ class ArticleRemote @Inject constructor(
         }
     }
 
+    /**
+     * Searches the remote data source for news articles that match the specified [searchQuery].
+     * Uses [dispatcher] to perform the operation on an I/O-bound coroutine dispatcher.
+     *
+     * @param searchQuery - the search query string to use when searching for articles.
+     * @return a [NewsResponseModel] object that contains the search results.
+     */
     override suspend fun searchNews(searchQuery: String): NewsResponseModel = dispatcher.io {
         val data = apiService.searchArticles(searchQuery)
         mapper.converterToNewsResponseModel(data)
