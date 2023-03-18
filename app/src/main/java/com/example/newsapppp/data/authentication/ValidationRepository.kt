c723cfcee6d49fb7cebb9a860f8b919d272007d0
Stas-8888 <life4musicrd@gmail.com>
2023-03-18 02:19:04 +0200

    add comments

--- app/src/main/java/com/example/newsapppp/data/authentication/ValidationRepository.kt
+++ app/src/main/java/com/example/newsapppp/data/authentication/ValidationRepository.kt
@@ -6,10 +6,19 @@ import com.example.newsapppp.core.resources.ProvideResourcesContract
 import com.example.newsapppp.domain.interactors.authentication.ValidationRepositoryContract
 import javax.inject.Inject
 
+/**
+ * Repository class for validating user input during authentication.
+ * @param provideResources An interface for accessing app resources.
+ */
 class ValidationRepository @Inject constructor(
     private val provideResources: ProvideResourcesContract
 ) : ValidationRepositoryContract {
 
+    /**
+     * Validates user input for email address.
+     * @param email User input for email address.
+     * @return A string indicating success or failure of the validation.
+     */
     override fun validateEmail(email: String): String {
         if (email.isEmpty()) {
             return provideResources.makeString(R.string.empty_email)
@@ -20,6 +29,11 @@ class ValidationRepository @Inject constructor(
         return provideResources.makeString(R.string.successful)
     }
 
+    /**
+     * Validates user input for password.
+     * @param passwordText User input for password.
+     * @return A string indicating success or failure of the validation.
+     */
     override fun validatePassword(passwordText: String): String {
         if (passwordText.isEmpty()) {
             return provideResources.makeString(R.string.empty_password)
@@ -36,6 +50,12 @@ class ValidationRepository @Inject constructor(
         return provideResources.makeString(R.string.successful)
     }
 
+    /**
+     * Validates user input for repeated password.
+     * @param password User input for password.
+     * @param repeatedPassword User input for repeated password.
+     * @return A string indicating success or failure of the validation.
+     */
     override fun validateRepeatedPassword(password: String, repeatedPassword: String): String {
         if (password != repeatedPassword) {
             return provideResources.makeString(R.string.different_password)
@@ -43,6 +63,11 @@ class ValidationRepository @Inject constructor(
         return provideResources.makeString(R.string.successful)
     }
 
+    /**
+     * Validates user input for full name.
+     * @param fullName User input for full name.
+     * @return A string indicating success or failure of the validation.
+     */
     override fun fullName(fullName: String): String {
         if (fullName.length < 6) {
             return provideResources.makeString(R.string.minimum_character)
