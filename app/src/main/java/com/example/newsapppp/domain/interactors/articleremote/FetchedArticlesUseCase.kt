34e074ed1d700f63cdc6ebd6933b0706397f2cb8
Stas-8888 <life4musicrd@gmail.com>
2023-03-21 13:06:16 +0200

    add comments

--- app/src/main/java/com/example/newsapppp/domain/interactors/articleremote/FetchedArticlesUseCase.kt
+++ app/src/main/java/com/example/newsapppp/domain/interactors/articleremote/FetchedArticlesUseCase.kt
@@ -6,9 +6,18 @@ import com.example.newsapppp.domain.model.ArticleModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * This use case fetches a paged list of articles from a remote data source.
+ * @param repo The repository that fetches the articles.
+ */
 class FetchedArticlesUseCase @Inject constructor(private val repo: ArticleRemoteContract) :
     BaseUseCaseSuspend<String, Flow<PagingData<ArticleModel>>> {
 
+    /**
+     * Invokes the use case and fetches a paged list of articles from the remote data source.
+     * @param data The category of articles to be fetched.
+     * @return A flow of paged article data from the remote data source.
+     */
     override suspend fun invoke(data: String): Flow<PagingData<ArticleModel>> {
         return repo.fetchedArticles(category = data)
     }
