8418f869b2d58725e46852a302821ec8d5d28518
Stas-8888 <life4musicrd@gmail.com>
2023-03-22 02:19:49 +0200

    add comments

--- app/src/main/java/com/example/newsapppp/data/remote/paging/ArticlePagingSource.kt
+++ app/src/main/java/com/example/newsapppp/data/remote/paging/ArticlePagingSource.kt
@@ -7,6 +7,13 @@ import com.example.newsapppp.data.remote.service.ApiService
 import com.example.newsapppp.domain.model.ArticleModel
 import retrofit2.HttpException
 
+/**
+ * A paging source for fetching news articles from an API.
+ * @param repository An instance of ApiService for fetching articles.
+ * @param countryCode A country code used to filter articles by country.
+ * @param category A category used to filter articles by topic.
+ * @param mapper A mapper used to map the response data to ArticleModel objects.
+ */
 class ArticlePagingSource(
     private val repository: ApiService,
     private val countryCode: String,
@@ -14,6 +21,11 @@ class ArticlePagingSource(
     private val mapper: NewsResponseMapper
 ) : PagingSource<Int, ArticleModel>() {
 
+    /**
+     * Load a page of articles from the API.
+     * @param params The load parameters, including the page key and page size.
+     * @return A LoadResult object containing the loaded data, as well as keys for the previous and next pages.
+     */
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ArticleModel> {
         return try {
             val currentPage = params.key ?: 1
@@ -35,6 +47,11 @@ class ArticlePagingSource(
 
     }
 
+    /**
+     * Get the key for the next page to be loaded after a refresh.
+     * @param state The current paging state.
+     * @return The key for the next page to be loaded.
+     */
     override fun getRefreshKey(state: PagingState<Int, ArticleModel>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
