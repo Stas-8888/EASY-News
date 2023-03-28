b82879c9e6d6c77b1e7c7948b2c81c7be2f4849e
Stas-8888 <life4musicrd@gmail.com>
2023-03-28 17:49:10 +0300

    add comments

--- app/src/main/java/com/example/newsapppp/presentation/screens/base/BaseFragment.kt
+++ app/src/main/java/com/example/newsapppp/presentation/screens/base/BaseFragment.kt
@@ -15,7 +15,12 @@ import com.example.newsapppp.presentation.extensions.returnToPreviousScreen
 import kotlinx.coroutines.flow.collectLatest
 
 /**
- * Base class for all Fragments.
+ * A base fragment class that provides common functionality for fragments.
+ *
+ * @param <State> The state of the fragment.
+ * @param <Action> The action that the fragment can perform.
+ * @param <VB> The type of the view binding class for the fragment.
+ * @param <VM> The type of the view model class for the fragment.
  */
 abstract class BaseFragment<State, Action, VB : ViewBinding, VM : BaseViewModel<State, Action>>(
     private val bindingInflater: (inflater: LayoutInflater) -> VB
@@ -46,22 +51,48 @@ abstract class BaseFragment<State, Action, VB : ViewBinding, VM : BaseViewModel<
         observeOnAction()
     }
 
+    /**
+     * Observes the state of the view model and updates the UI accordingly.
+     */
     private fun observeOnState() = launchWhenStarted {
         viewModel.state.collectLatest { state ->
             observerState(state)
         }
     }
 
+    /**
+     * Observes the actions of the view model and updates the UI accordingly.
+     */
     private fun observeOnAction() = launchWhenStarted {
         viewModel.action.collectLatest { actions ->
             observerAction(actions)
         }
     }
 
+    /**
+     * Called when a click event occurs in the UI.
+     */
     abstract fun onClickListener()
+
+    /**
+     * Called when the state of the view model changes.
+     *
+     * @param state The new state of the view model.
+     */
     abstract fun observerState(state: State)
+
+    /**
+     * Called when an action is performed by the view model.
+     *
+     * @param actions The action performed by the view model.
+     */
     abstract fun observerAction(actions: Action)
 
+    /**
+     * Initializes the toolbar with animation and navigation functionality.
+     *
+     * @param toolbar The toolbar to be initialized.
+     */
     protected fun initialToolBar(toolbar: androidx.appcompat.widget.Toolbar) {
         toolbar.startAnimation(TranslateAnimation(1000f, 0f, 0f, 0f).apply {
             duration = 800
@@ -81,6 +112,11 @@ abstract class BaseFragment<State, Action, VB : ViewBinding, VM : BaseViewModel<
         }
     }
 
+    /**
+     *Sets up a RecyclerView with the provided [recyclerView] and [baseAdapter].
+     *@param recyclerView The RecyclerView to be set up.
+     *@param baseAdapter The RecyclerView adapter to be used.
+     */
     protected fun setupRecyclerView(
         recyclerView: RecyclerView,
         baseAdapter: RecyclerView.Adapter<*>
