a50555aaff7eee3865c198d033b8a11801c55ce6
Stas-8888 <life4musicrd@gmail.com>
2023-04-01 00:25:59 +0300

    add comments

--- app/src/main/java/com/example/newsapppp/data/remote/interceptor/RestErrorInterceptor.kt
+++ app/src/main/java/com/example/newsapppp/data/remote/interceptor/RestErrorInterceptor.kt
@@ -9,8 +9,23 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
+/**
+ * This class represents an interceptor used to handle errors in HTTP requests.
+ * It implements the Interceptor interface provided by the OkHttp library.
+ * The RestErrorInterceptor intercepts the HTTP response and checks the response code.
+ * If the code is one of the pre-defined error codes (400, 429, or 500), it emits an error message
+ * using the ErrorsInterceptorContract.
+ * @param errors An instance of ErrorsInterceptorContract used to emit error messages.
+ */
 class RestErrorInterceptor(private var errors: ErrorsInterceptorContract) : Interceptor {
 
+    /**
+     * This method intercepts the HTTP request and response.
+     * If the response code is one of the pre-defined error codes, it emits an error message
+     * using the ErrorsInterceptorContract.
+     * @param chain An instance of Interceptor.Chain that represents the request and response chain.
+     * @return The HTTP response from the server.
+     */
     override fun intercept(chain: Interceptor.Chain): Response {
         val request: Request = chain.request()
         val response = chain.proceed(request)
@@ -34,4 +49,3 @@ class RestErrorInterceptor(private var errors: ErrorsInterceptorContract) : Inte
         return response
     }
 }
-
