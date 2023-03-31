651723e2c8e23d53dfe08e974603681f1d8bec4b
Stas-8888 <life4musicrd@gmail.com>
2023-04-01 00:30:29 +0300

    add comments

--- app/src/main/java/com/example/newsapppp/data/remote/interceptor/ErrorsInterceptor.kt
+++ app/src/main/java/com/example/newsapppp/data/remote/interceptor/ErrorsInterceptor.kt
@@ -4,14 +4,27 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import javax.inject.Inject
 
+/**
+ * This class implements the [ErrorsInterceptorContract] interface to provide an
+ * implementation for error handling in a flow.
+ *
+ * @property flow The [MutableSharedFlow] used to emit error codes.
+ * @constructor Creates an instance of [ErrorsInterceptor].
+ */
 class ErrorsInterceptor @Inject constructor() : ErrorsInterceptorContract {
     val flow = MutableSharedFlow<Int>()
 
+    /**
+     * @return The [SharedFlow] used to emit error codes.
+     */
     override fun errorsInterceptor(): SharedFlow<Int> {
         return flow
     }
 
-    override suspend fun emitError(error: Int){
+    /**
+     * @param error The error code to be emitted to the [flow].
+     */
+    override suspend fun emitError(error: Int) {
         flow.emit(error)
     }
 }
