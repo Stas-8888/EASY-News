6d8130897c19c3bb6908556891b34a9eed0b03ec
Stas-8888 <life4musicrd@gmail.com>
2023-05-29 10:41:12 +0300

    add Firebase Crashlytics

--- app/src/main/java/com/example/newsapppp/presentation/screens/activity/MainActivity.kt
+++ app/src/main/java/com/example/newsapppp/presentation/screens/activity/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.newsapppp.presentation.extensions.slideDownAnimation
 import com.example.newsapppp.presentation.extensions.slideUpAnimation
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -37,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun observe() = lifecycleScope.launchWhenStarted {
+    private fun observe() = lifecycleScope.launch {
         with(binding) {
             viewModel.state.collectLatest {
                 when (it) {
