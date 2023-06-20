e98616263b9ea187b111d28c75ab3563b15d6fc1
Stas-8888 <life4musicrd@gmail.com>
2023-06-20 23:35:26 +0300

    add logger

--- app/src/main/java/com/example/newsapppp/presentation/screens/CurrencyApp.kt
+++ app/src/main/java/com/example/newsapppp/presentation/screens/CurrencyApp.kt
@@ -3,7 +3,12 @@ package com.example.newsapppp.presentation.screens
 import android.app.Application
 import androidx.work.*
 import com.example.newsapppp.presentation.notification.NotificationWorker
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 
@@ -22,7 +27,27 @@ class CurrencyApp : Application() {
      */
     override fun onCreate() {
         super.onCreate()
+        setupTimber()
         notificationWork()
+    }
+
+    private fun setupTimber() {
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(true) // (Optional) Whether to show thread info or not. Default true
+            .methodCount(1) // (Optional) How many method line to show. Default 2
+            .methodOffset(5) // Set methodOffset to 5 in order to hide internal method calls
+            .tag("LOGGER") // To replace the default PRETTY_LOGGER tag with a dash (-).
+            .build()
+
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(
+                priority: Int, tag: String?, message: String, t: Throwable?
+            ) {
+                Logger.log(priority, "-$tag", message, t)
+            }
+        })
     }
 
     /**
