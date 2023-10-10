83b6f79d880775285d621793a80a41c52e48072a
Stas-8888 <life4musicrd@gmail.com>
2023-10-10 18:21:47 +0300

    update dependencies

--- app/src/test/java/com/example/newsapppp/domain/use_case/articlecache/InsertArticleUseCaseTest.kt
+++ app/src/test/java/com/example/newsapppp/domain/use_case/articlecache/InsertArticleUseCaseTest.kt
@@ -1,24 +1,37 @@
 package com.example.newsapppp.domain.use_case.articlecache
 
+import com.example.newsapppp.domain.model.ArticleModel
 import com.example.newsapppp.domain.repository.ArticleCacheRepository
 import com.example.newsapppp.domain.use_case.articles.cache.InsertArticleUseCase
-import com.example.newsapppp.domain.model.ArticleModel
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
-import org.mockito.kotlin.mock
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyNoMoreInteractions
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
 
 class InsertArticleUseCaseTest {
 
-    private val mockRepo = mock<ArticleCacheRepository>()
+    @Mock
+    private lateinit var mockRepository: ArticleCacheRepository
+
+    private lateinit var useCase: InsertArticleUseCase
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        useCase = InsertArticleUseCase(mockRepository)
+    }
 
     @Test
-    fun `inserted article should be returned in getAllArticles`() = runBlocking {
-        val mockArticleModel = ArticleModel(
+    fun `insertArticle should return success`() = runBlocking {
+        // Given
+        val article = ArticleModel(
             title = "Mock article title",
             description = "Mock article description",
             url = "http://example.com",
@@ -27,18 +40,123 @@ class InsertArticleUseCaseTest {
             content = "Mock article content",
             author = "Mock author"
         )
+//        `when`(mockRepository.insertArticle(article)).thenReturn(Unit)
 
-        // Mock the repository to return a flow of a list with the mock article
-        Mockito.`when`(mockRepo.getAllArticles()).thenReturn(flowOf(listOf(mockArticleModel)))
+        // When
+        val result = useCase.invoke(article)
 
-        // Call the use case to insert the mock article
-        val insertArticleUseCase = InsertArticleUseCase(mockRepo)
-        insertArticleUseCase.invoke(mockArticleModel)
-
-        // Verify that the mock article is returned in getAllArticles
-        val expected = listOf(mockArticleModel)
-        val actual = mockRepo.getAllArticles().first()
-
-        assertEquals(expected, actual)
+        // Then
+        assertNotNull(result)
+        verify(mockRepository, times(1)).insertArticle(article)
+        verifyNoMoreInteractions(mockRepository)
     }
+
+    @Test(expected = Exception::class)
+    fun `insertArticle with invalid data should throw exception`() = runBlocking {
+        // Given
+        val invalidArticle = ArticleModel(
+            title = "Mock article title",
+            description = "Mock article description",
+            url = "http://example.com",
+            urlToImage = "http://example.com/image.jpg",
+            publishedAt = "2023-03-08T12:00:00Z",
+            content = "Mock article content",
+            author = "Mock author"
+        )
+        `when`(mockRepository.insertArticle(invalidArticle)).thenThrow(Exception("Invalid data"))
+
+        // When
+        useCase.invoke(invalidArticle)
+
+        // Then
+        // Expecting an exception to be thrown
+    }
+
+    @Test(expected = Exception::class)
+    fun `insertArticle with null data should throw exception`() = runBlocking {
+        // Given
+        val nullArticle: ArticleModel? = null
+        `when`(mockRepository.insertArticle(nullArticle!!)).thenThrow(Exception("Null data"))
+
+        // When
+        useCase.invoke(nullArticle)
+
+        // Then
+        // Expecting an exception to be thrown
+    }
+
+
+
+//    private val mockRepo = mock<ArticleCacheRepository>()
+//
+//    @Test
+//    fun `inserted article should be returned in getAllArticles`() = runBlocking {
+//        val mockArticleModel = ArticleModel(
+//            title = "Mock article title",
+//            description = "Mock article description",
+//            url = "http://example.com",
+//            urlToImage = "http://example.com/image.jpg",
+//            publishedAt = "2023-03-08T12:00:00Z",
+//            content = "Mock article content",
+//            author = "Mock author"
+//        )
+//
+//        // Mock the repository to return a flow of a list with the mock article
+//        Mockito.`when`(mockRepo.getAllArticles()).thenReturn(flowOf(listOf(mockArticleModel)))
+//
+//        // Call the use case to insert the mock article
+//        val insertArticleUseCase = InsertArticleUseCase(mockRepo)
+//        insertArticleUseCase.invoke(mockArticleModel)
+//
+//        // Verify that the mock article is returned in getAllArticles
+//        val expected = listOf(mockArticleModel)
+//        val actual = mockRepo.getAllArticles().first()
+//
+//        assertEquals(expected, actual)
+//    }
+//
+//    @Test
+//    fun insertArticleUseCase() = runBlocking {
+//        val mockArticleModel = ArticleModel(
+//            title = "Mock article title",
+//            description = "Mock article description",
+//            url = "http://example.com",
+//            urlToImage = "http://example.com/image.jpg",
+//            publishedAt = "2023-03-08T12:00:00Z",
+//            content = "Mock article content",
+//            author = "Mock author"
+//        )
+//
+//        val fakeRepo = mockRepository
+//        val insertArticleUseCase = InsertArticleUseCase(fakeRepo)
+//
+//        insertArticleUseCase.invoke(mockArticleModel)
+//
+////        val expected = listOf(mockArticleModel)
+//        val expected = fakeRepo.insertArticle(mockArticleModel)
+//        val actual = fakeRepo.getAllArticles().first()
+//
+//        assertEquals(expected, actual)
+//    }
+//
+//    private class FakeMainRepository : ArticleCacheRepository {
+//        private val articles = mutableListOf<ArticleModel>()
+//
+//        override suspend fun insertArticle(article: ArticleModel) {
+//            articles.add(article)
+//        }
+//
+//        override suspend fun deleteArticle(article: ArticleModel) {
+//            articles.remove(article)
+//        }
+//
+//        override fun getAllArticles(): Flow<List<ArticleModel>> = flow {
+//            emit(articles)
+//        }
+//
+//        override suspend fun deleteAllArticle() {
+//            articles.clear()
+//        }
+//    }
+
 }
